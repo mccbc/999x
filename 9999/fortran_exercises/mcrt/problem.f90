4b82265c3be26171744c2002f0415ec75c34a2fc
mccbc <cmcclellan1010@gmail.com>
2020-06-08 14:05:02 -0400

    fixed bad transformations in problem.f90

--- 9999/fortran_exercises/mcrt/problem.f90
+++ 9999/fortran_exercises/mcrt/problem.f90
@@ -46,8 +46,11 @@ subroutine beam(pos, dir)
   ! IMPACT PARAMETER
   b = 2.0d0
 
-  pos(1) = reg(2, 1) - 1e-3 ! Set r coordinate to be rmax
-  pos(2) = asin(b * sqrt(zeta) / reg(2, 1)) ! Distribute theta uniformly over the solid angle covered by the impact parameter
+  pos(1) = reg(2) - 1e-3 ! Set r coordinate to be rmax
+  pos(2) = asin(b * sqrt(zeta) / reg(2)) ! Distribute theta uniformly over the solid angle covered by the impact parameter
+  if (pos(2) .lt. 0.d0) then
+    pos(2) = pos(2) + pi
+  endif
   pos(3) = xi * 2.0 * pi  ! Distribute phi uniformly over a circle
 
   dir = reshape((/-cos(pos(2)), sin(pos(2)), 0.d0/), shape(dir))
@@ -60,13 +63,15 @@ subroutine step(pos, dir, region, exitflag, nsteps)
   real (dp), dimension(3), intent(inout) :: pos, dir
   real (dp), dimension(6), intent(in) :: region
   real (dp), dimension(3, 3) :: tran, itran, coord, icoord
+  real (dp), dimension(3) :: cpos, cdir, dist_test
   real (dp) :: xi, zeta, omega, psi, chi
   real (dp) :: x, y, z
-  real (dp) :: tau, s
+  real (dp) :: tau, s, dot, magsq
+  integer :: i, j
   integer, intent(inout) :: nsteps
   logical, intent(out) :: exitflag
 
-  chi = 1e-2
+  chi = 1.d0
 
   ! Generate an optical depth through which to travel
   call random_number(xi)
@@ -74,17 +79,19 @@ subroutine step(pos, dir, region, exitflag, nsteps)
 
   ! Update photon's position in cartesian coords
   call transf(pos, tran, itran, coord, icoord)
-  pos = matmul(transpose(icoord), pos)
-  dir = matmul(transpose(itran), dir)
-  pos = pos + chi * tau * dir
-  pos = matmul(transpose(coord), pos)
-  dir = matmul(transpose(tran), dir)
+
+  !print *, 'pos: ', pos
+  cpos = ((/pos(1)*sin(pos(2))*cos(pos(3)), pos(1)*sin(pos(2))*sin(pos(3)), pos(1)*cos(pos(2))/))
+  !print *, 'cpos: ', cpos
+  cdir = matmul(transpose(itran), dir)
+  dist_test = cpos + chi * tau * cdir
+  !print *, 'distance: ', norm2(dist_test)
   nsteps = nsteps + 1
 
-  ! Check if photon is still within domain
-  if (((region(1) .le. pos(1)) .and. (pos(1) .le. region(2)))        &
-      .and. ((region(3) .le. pos(2)) .and. (pos(2) .le. region(4)))  &
-      .and. ((region(5) .le. pos(3)) .and. (pos(3) .le. region(6)))) then
+  if (norm2(dist_test) .le. region(2)) then
+
+
+    cpos = cpos + chi * tau * cdir
 
     ! Generate a new direction in cartesian coordinates
     call random_number(zeta)
@@ -95,19 +102,52 @@ subroutine step(pos, dir, region, exitflag, nsteps)
     y = 2.d0 * omega - 1.d0
     z = 2.d0 * psi - 1.d0
 
-    dir = reshape((/x, y, z/), shape(dir))
-    dir = dir / norm2(dir)
+    cdir = reshape((/x, y, z/), shape(cdir))
+    cdir = cdir / norm2(cdir)
+    
+    pos(1) = norm2(cpos)
+    pos(2) = acos(cpos(2) / pos(1))
+    pos(3) = atan2(cpos(2), cpos(1))
+    if (pos(3) .lt. 0.d0) then
+      pos(3) = pos(3) + 2.d0*pi
+    endif
+
+    !print *, 'cartesian pos: ', pos
+
     call transf(pos, tran, itran, coord, icoord)
-    dir = matmul(transpose(tran), dir) ! Should still be normalized, right?
+    dir = matmul(transpose(tran), cdir)
     exitflag = .false.
 
   else
-    ! Step back to when the photon was within the domain
     ! Solve for the right distance to travel to reach r = rmax
     ! Travel that distance, update pos and dir vectors
-    pos = pos - chi * tau * dir
-    s = (region(2) - pos(1))/dir(1)
-    pos = pos + s * dir
+    
+    dot = dot_product(cpos, cdir)
+    magsq = norm2(cpos)**2.d0
+    s = (-dot + sqrt(dot**2.d0 - 4.d0*(magsq - region(2)**2.d0)))/2.d0
+
+    !print *, 'dot: ', dot
+    !print *, 'dotsq :', dot**2.d0
+    !print *, 'cpos: ', cpos
+    !print *, 'magsq: ', magsq, cpos(1)**2. + cpos(2)**2. + cpos(3)**2.
+    !print *, 'magsq - r^2: ', magsq - region(2)**2.d0
+    !print *, 'arg sqrt: ', dot**2.d0 - 4.d0*(magsq - region(2)**2.d0)
+    !print *, 'cartesian pos: ', cpos
+
+    cpos = cpos + s * cdir
+    !print *, 'cartesian pos after step: ', cpos
+
+    pos(1) = norm2(cpos)
+    pos(2) = acos(cpos(2) / pos(1))
+    pos(3) = atan2(cpos(2), cpos(1))
+    if (pos(3) .lt. 0.d0) then
+      pos(3) = pos(3) + 2.d0*pi
+    endif
+
+    !print *, 'polar pos after step: ', pos
+
+    call transf(pos, tran, itran, coord, icoord)
+    dir = matmul(transpose(tran), cdir)    
     
     ! Set exit flag so that main loop knows to save the outputs to a file
     exitflag = .true.
@@ -119,7 +159,7 @@ program problem
   implicit none
   real (dp), dimension(3) :: pos, dir
   logical :: exitflag = .false.
-  integer :: i, n=100, nsteps=0
+  integer :: i, n=1000, nsteps=0
 
   ! GAS REGION SPECIFICATION (simple bounds in code units)
   ! x1min, x1max, x2min, x2max, x3min, x3max
