5e4b6c035f694809c433c6b8729ee6632391848a
mccbc <cmcclellan1010@gmail.com>
2020-08-14 13:59:48 -0400

    fixes to J-r_sigma_t.py

--- 9999/lya_analytic/J_r_sigma_t.py
+++ 9999/lya_analytic/J_r_sigma_t.py
@@ -2,11 +2,11 @@ import numpy as np
 from solutions.util import j0
 from tqdm import tqdm
 
-def evaluate_Jrt(path, r, t, n, omega):
+def evaluate_Jrt(path, r, sigma, t, n, omega):
 
     d_omega = np.diff(omega)[0]
     R = r[-1]
-    Jrt = np.zeros((len(r), len(t)))
+    Jrt = np.zeros((len(r), len(sigma), len(t)))
 
     # Sum & discretized integral
 
@@ -20,23 +20,24 @@ def evaluate_Jrt(path, r, t, n, omega):
                     kappa_n = n[k] * np.pi / R
 
                     # Load in data for this n and omega
-                    with np.load(path+'J_omega{}_n{}.npy'.format(l, k)) as Jdump:
-                        J_n_sigma_omega = Jdump[:, 0] + 1j*Jdump[:, 1]
+                    Jdump = np.load(path+'J_omega{}_n{}.npy'.format(l, k)):
+                    J_n_sigma_omega = Jdump[:, 0] + 1j*Jdump[:, 1]
 
                     # Eq 34
-                    omega_integral += d_omega / (2.*np.pi) * J_n_sigma_omega * j0(kappa_n / r[j]) * np.exp(-1j*omega[l]*t[i])
+                    omega_integral += d_omega / (2.*np.pi) * J_n_sigma_omega * j0(kappa_n, r[j]) * np.exp(-1j*omega[l]*t[i])
                 n_sum += omega_integral
-            Jrt[j, i] = n_sum
+            Jrt[j, :, i] = n_sum
     return Jrt
 
 
 if __name__ == "__main__":
-    r = np.linspace(0, 1e11, 1e3)
-    t = np.linspace(0, 1e3, 1e3)
+    r = np.linspace(1e-1, 1e11, int(1e3))
+    t = np.linspace(0, 1e3, int(1e3))
     
     path = './outputs/n8_sigma1e6_omega64/'
     n = np.load(path+'n_grid.npy')
     omega = np.load(path+'omega_grid.npy')
+    sigma = np.load(path+'sigma_grid.npy')
 
-    Jrt = evaluate_Jrt(path, r, t, n, omega)
+    Jrt = evaluate_Jrt(path, r, sigma, t, n, omega)
     np.save(path+'Jrt.npy', Jrt)
