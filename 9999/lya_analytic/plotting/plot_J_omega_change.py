1f7cf8a7de0cf90234b8fb15ddf27c86706e5173
mccbc <cmcclellan1010@gmail.com>
2020-08-27 15:27:58 -0400

    added new plotting script, reworked J omega change plot to use new filesystem

--- 9999/lya_analytic/plotting/plot_J_omega_change.py
+++ 9999/lya_analytic/plotting/plot_J_omega_change.py
@@ -1,28 +1,28 @@
 import numpy as np
+import h5py
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 
-path = './outputs/n8_sigma1e6_omega64/'
-n = np.load(path+'n_grid.npy')
-omega = np.load(path+'omega_grid.npy')
-sigma = np.load(path+'sigma_grid.npy')
+data = h5py.File('../outputs/n16_sigma100000_omega128.hdf5', 'r')
 
-# Sum & discretized integral
+n = data['n']
+omega = data['omega']
+sigma = data['sigma']
+
 for k in range(len(n)):
 
     plt.figure()
     colors = pl.cm.jet(np.linspace(0, 1, len(omega)))
-    for l in range(len(omega)):
+    for l in [0, 1, 2, 3, 4, 5]: #range(len(omega)):
 
         # Load in data for this n and omega
-        Jdump = np.load(path+'J_omega{}_n{}.npy'.format(l, k))
-        J_real = Jdump[:, 0]
-        J_imag = Jdump[:, 1]
+        J = data['J_omega{}_n{}'.format(l, k)][:]
 
-        plt.plot(sigma, J_real, '-', alpha=0.25, color=colors[l])
-        plt.plot(sigma, J_real, '--', alpha=0.25, color=colors[l])
+        plt.plot(sigma, np.abs(J.real), '-', alpha=0.25, color=colors[l])
+#        plt.plot(sigma, J.imag, '--', alpha=0.25, color=colors[l])
 
     plt.title('J Coefficients for n={}'.format(n[k]))
     plt.xlabel('sigma')
     plt.ylabel('J(n, sigma, omega)')
+    plt.yscale('log')
     plt.show()
