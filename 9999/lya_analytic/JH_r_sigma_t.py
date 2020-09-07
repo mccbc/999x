added colorbar to omega change plots, fixed sign error in equation for flux

9999/lya_analytic/JH_r_sigma_t.py
@@ -36,10 +36,10 @@ def evaluate_J_H(inputname, r, sigma, t, outputname, axis=1, mp=True):
     output.close()
 
     if mp:
-        pb = tqdm(total=len(t)*len(r)*len(aux_variables[0])*len(aux_variables[1]))
+        pb = tqdm(total=len(t)*len(r)*len(n)*len(omega)*len(sigma))
 
         def save_queue_rsigmat(result):
-            pb.update()
+            pb.update(len(prim_variable))
             Jsetname, Hsetname, J, H, fname = result
             output = h5py.File(fname, 'a')
             try:
@@ -92,9 +92,9 @@ def evaluate_J_H(inputname, r, sigma, t, outputname, axis=1, mp=True):
                             r_index, sigma_index, t_index = iters_ord
 
                             # Eq 34
-                            J[k] += d_omega / (2.*np.pi) * J_interp(sigma[sigma_index]) * j0(kappa_n, r[r_index]) * np.exp(-1j*omega[m]*t[t_index])
+                            J[k] += 2. * d_omega / (2.*np.pi) * J_interp(sigma[sigma_index]) * j0(kappa_n, r[r_index]) * np.exp(-1j*omega[m]*t[t_index])
                             j0_prime = np.cos(kappa_n*r[r_index])/r[r_index] - np.sin(kappa_n*r[r_index])/kappa_n/r[r_index]**2.
-                            H[k] += d_omega / (2.*np.pi) * J_interp(sigma[sigma_index]) * j0_prime * np.exp(-1j*omega[m]*t[t_index])
+                            H[k] += - 2. * d_omega / (2.*np.pi) * J_interp(sigma[sigma_index]) * j0_prime * np.exp(-1j*omega[m]*t[t_index])
                             pb.update()
                         iterator = iters_ord.pop(axis)
                         Jsetname = 'J_{}{}_{}{}'.format(names[0], iters_ord[0], names[1], iters_ord[1])

9999/lya_analytic/check_norm.py
@@ -1,22 +1,23 @@
 import h5py
 import numpy as np
 
-n_r, n_sigma, n_t = 1, 1000, 1000
+n_r, n_sigma, n_t = 1, 100, 100
 a = h5py.File('./outputs/r{}_sigma{}_t{}.hdf5'.format(n_r, n_sigma, n_t), 'r')
 
 r = a['r'][:]
 sigma = a['sigma'][:]
 t = a['t'][:]
 
-H_t = np.zeros(len(t))
-d_sigma = np.diff(sigma)[0]
-for i in range(len(sigma)):
-    Hdump = a['H_r0_sigma{}'.format(i)][:]
-    H_t += d_sigma * Hdump * 4. * np.pi * r**2.
-
-H = 0.
+H_sigma = np.zeros(len(sigma))
 dt = np.diff(t)[0]
 for j in range(len(t)):
-    H += dt * H_t
+    H_dump = a['H_r0_t{}'.format(j)][:]
+    H_sigma += dt * H_dump.real
+
+H = 0.
+d_sigma = np.diff(sigma)[0]
+for i in range(len(sigma)):
+    H += d_sigma * H_sigma[i] * 4. * np.pi * r**2.
+
 
 print("summed H = {}".format(H))

9999/lya_analytic/plotting/plot_J_omega_change.py
@@ -3,7 +3,7 @@ import h5py
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 
-data = h5py.File('../outputs/n16_sigma100000_omega128.hdf5', 'r')
+data = h5py.File('../outputs/n8_sigma100000_logomega128.hdf5', 'r')
 
 n = data['n']
 omega = data['omega']
@@ -11,7 +11,7 @@ sigma = data['sigma']
 
 for k in range(len(n)):
 
-    plt.figure()
+    fig = plt.figure()
     colors = pl.cm.jet(np.linspace(0, 1, len(omega)))
     for l in range(len(omega)):
 
@@ -20,6 +20,9 @@ for k in range(len(n)):
 
         plt.plot(sigma, np.abs(J.real), '-', alpha=0.25, color=colors[l])
 #        plt.plot(sigma, J.imag, '--', alpha=0.25, color=colors[l])
+    sm = plt.cm.ScalarMappable(cmap=pl.cm.jet, norm=plt.Normalize(vmin=min(omega), vmax=max(omega)))
+    cbar = fig.colorbar(sm)
+    cbar.ax.set_ylabel('omega', rotation=90)
 
     plt.title('J Coefficients for n={}'.format(n[k]))
     plt.xlabel('sigma')