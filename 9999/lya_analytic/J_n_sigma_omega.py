6eb9896e816644dbee20fe9ec4d7bbe6e55e7fe7
mccbc <cmcclellan1010@gmail.com>
2020-09-23 11:29:56 -0400

    set up J_n_sigma_omega for convergence runs

--- 9999/lya_analytic/J_n_sigma_omega.py
+++ 9999/lya_analytic/J_n_sigma_omega.py
@@ -26,17 +26,17 @@ dt = 0.1*tdiff
 
 # Number of omega points in grid
 N_omegas = 128
-N_ns = 4
+N_ns = 8
 
 # Create grids
-#omega_grid = np.linspace(0, 2*np.pi/dt, N_omegas)
-omega_grid = np.logspace(np.log(1e-3), np.log10(2*np.pi/dt), N_omegas-1)
-omega_grid = np.insert(omega_grid, 0, 0.)
+omega_grid = np.linspace(0, 2*np.pi/dt, N_omegas)
+#omega_grid = np.logspace(np.log(1e-3), np.log10(2*np.pi/dt), N_omegas-1)
+#omega_grid = np.insert(omega_grid, 0, 0.)
 n_grid = np.arange(1, N_ns+1, 1)
 sigma_grid = p.sigma_grid
 
 # Create output hdf5 file
-fname = '/LyraShared/bcm2vn/outputs/lya_analytic/n{}_widesigma{}_logomega{}.hdf5'.format(N_ns, len(sigma_grid), N_omegas)
+fname = '/LyraShared/bcm2vn/outputs/lya_analytic/n{}_sigma{}_omega{}_convergence.hdf5'.format(N_ns, len(sigma_grid), N_omegas)
 #fname = './outputs/n{}_widesigma{}_logomega{}.hdf5'.format(N_ns, len(sigma_grid), N_omegas)
 
 pb = tqdm(total=len(omega_grid)*len(n_grid))
