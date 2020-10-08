836b551e4fd5f291510aab52b988c59684a87b37
mccbc <cmcclellan1010@gmail.com>
2020-10-08 15:24:16 -0400

    tried to use mpmath integrator

--- 9999/lya_analytic/overflow_test.py
+++ 9999/lya_analytic/overflow_test.py
@@ -12,10 +12,14 @@ def _integrator(x, y):
     return [y1, y2]
 
 
-sol = m.odefun(_integrator, -1000, [1., 0.], verbose=True, tol=m.mpf('1e600'))
+sol = m.odefun(_integrator, -1000, [1., 0.], verbose=True, tol=m.mpf('1e600')) # Tol is arbitrarily high --- otherwise it takes FOREVER to obtain solutions
 
 x_eval = np.linspace(-1000, -200)
-plt.plot(x_eval, [sol(x) for x in x_eval])
+plt.plot(x_eval, [sol(x) for x in x_eval], marker='o')
 sols = [sol(x)[1] for x in x_eval]
-pdb.set_trace()
+
+print('Solution at x=-200:', sol(-200))
+
+plt.show()
+
 
