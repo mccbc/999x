1d3cb4203779fb5dd91123dff97c43bd4da088aa
mccbc <cmcclellan1010@gmail.com>
2020-10-08 14:37:51 -0400

    added demonstration of odefun and mpf arbitrary-precision floats

---
+++ 9999/lya_analytic/overflow_test.py
@@ -0,0 +1,21 @@
+import mpmath as m
+import pdb
+import matplotlib.pyplot as plt
+import numpy as np
+
+def _integrator(x, y):
+    (y2, y1) = y
+
+   # y2 = dy/dx
+   # y1 = y
+
+    return [y1, y2]
+
+
+sol = m.odefun(_integrator, -1000, [1., 0.], verbose=True, tol=m.mpf('1e600'))
+
+x_eval = np.linspace(-1000, -200)
+plt.plot(x_eval, [sol(x) for x in x_eval])
+sols = [sol(x)[1] for x in x_eval]
+pdb.set_trace()
+
