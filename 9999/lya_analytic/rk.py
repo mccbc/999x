a8060f4a6403461cdfeb7304b888a50b7347d497
mccbc <cmcclellan1010@gmail.com>
2020-10-08 17:54:40 -0400

    removed print statements from rk

--- 9999/lya_analytic/rk.py
+++ 9999/lya_analytic/rk.py
@@ -31,47 +31,41 @@ def rk(f, bounds, ivs, dt=1e-1, dt_min=1e-3,
 
     while (t < bounds[1]):
 
-        # Calculate partial steps.
+        # Calculate normal step
         k1 = f(t, x)
         k2 = f(t + dt / 2, x + dt * k1 / 2)
         k3 = f(t + dt / 2, x + dt * k2 / 2)
         k4 = f(t + dt, x + dt * k3)
-        # Combine partial steps.
         step_x = x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
-        # Calculate partial steps.
+        # Calculate half step
         k2 = f(t + dt / 4, x + dt * k1 / 4)
         k3 = f(t + dt / 4, x + dt * k2 / 4)
         k4 = f(t + dt / 2, x + dt * k3 / 2)
-        # Combine partial steps.
         half_step_x = x + dt / 12 * (k1 + 2 * k2 + 2 * k3 + k4)
 
-        # Calculate partial steps.
+        # Calculate double step
         k2 = f(t + dt, x + dt * k1)
         k3 = f(t + dt, x + dt * k2)
         k4 = f(t + 2 * dt, x + 2 * dt * k3)
-        # Combine partial steps.
         dble_step_x = x + dt / 3 * (k1 + 2 * k2 + 2 * k3 + k4)
 
         # Use a fixed step size if any x smaller than the x tolerance.
         if (abs(step_x) < x_tol).any():
             if (dt != dt_min):
-                print("New step size", dt_min)
                 dt = dt_min
             new_x = step_x
         else:
             if (abs(step_x - half_step_x) / abs(step_x) > dx_max).any():
                 # Error is too large for one or more x; decrease step size.
                 dt = dt / 2
-                print("New step size", dt)
                 new_x = half_step_x
             elif (abs(step_x - dble_step_x) / abs(step_x) < dx_min).all():
                 # Larger error is acceptable for all x; increase step size.
                 dt = dt * 2
-                print("New step size", dt)
                 new_x = dble_step_x
             else:
-                new_x = step_x  # This step size is just right.
+                new_x = step_x 
 
         x = new_x
         t = t + dt
@@ -94,7 +88,7 @@ if __name__ == '__main__':
        # x2 = dx/dt
        # x1 = x
 
-        print(t, x1)
+       # print(t, x1)
         return np.array([x1, x2])
 
     sol = rk(_integrator, [-1000, 0], [1., 0.])
