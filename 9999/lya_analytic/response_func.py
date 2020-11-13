99f43913127b8e8ffdd86b5cef7981ad02f4aa00
mccbc <cmcclellan1010@gmail.com>
2020-11-13 15:04:27 -0500

    adding response function script to see how the J coefficients depend on omega at specific sigma

---
+++ 9999/lya_analytic/response_func.py
@@ -0,0 +1,29 @@
+import h5py
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = h5py.File('./outputs/Jnso/n8_sigma10000_omega64_rk_debug.hdf5', 'r')
+
+sigma_indices = [4999, 5000, 6000]
+
+n = a['n'][:]
+omega = a['omega'][:]
+sigma = a['sigma'][:]
+
+for j in range(len(sigma_indices)):
+    sigma_index = sigma_indices[j]
+    sigma_val = sigma[sigma_index]
+    response = []
+    for i in range(len(omega)):
+        response.append(a['J_omega{}_n0'.format(i)][sigma_index])
+
+    val, exp = '{:.1e}'.format(a['sigma'][sigma_index]).split('e+')
+    plt.plot(omega, np.abs(response), marker='o', ms=2, alpha=0.5, label='$\sigma='+val+'\\times 10^{'+str(int(exp))+'}$')
+
+plt.ylabel('$|J (n=0, \sigma, \omega)|$')
+plt.yscale('log')
+plt.xlabel('$\omega$')
+plt.legend()
+plt.show()
